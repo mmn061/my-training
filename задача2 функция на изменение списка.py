54fc6860282877e53a6eef49c5f5bf192b17be7d
Mikhail <melnikov_mn@mail.ru>
2021-10-07 22:29:31 +0300

    задача создание изменение списка

--- задача2 функция на изменение списка.py
+++ задача2 функция на изменение списка.py
@@ -1,9 +1,18 @@
-lst = [1, 2, 3, 4, 5, 6]
-
+lst = [10, 5, 8, 3]
 
 
 def modify_list(l):
     lstm = []
-    for i in lst:
+    for i in l:
         if i % 2 == 0:
-            lstm += i
+            lstm.append(int(i / 2))
+    l.clear()
+    for j in lstm:
+        l.append(j)
+
+
+print(modify_list(lst))  # None
+print(lst)  # [1, 2, 3]
+
+modify_list(lst)
+print(lst)  # [1]
